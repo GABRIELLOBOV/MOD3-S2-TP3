6aaf01f939558c2bfa7f891442ad866ca51036f9
GABRIELLOBOV <lobovarela@gmail.com>
2025-06-23 08:49:18 -0300

    Add files via upload

---
+++ src/config/dbConfig.mjs
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+
+export async function connectDB () {
+    try {      
+ 
+//mongoose.connect('mongodb://localhost:3000'), {
+
+//const mongoose = require('mongoose'); 
+
+// Conexión a MongoDB con las opciones correctamente configuradas
+       await mongoose.connect('mongodb+srv://grupo-01:grupo01@cursadanodejs.ls9ii.mongodb.net/Node-js', {
+        //mongoose.connect('mongodb://localhost:3000'), {    
+       //useNewUrlParser: true,
+          //  useUnifiedTopology: true
+        });
+
+//.then(() => 
+    console.log('Conexión exitosa a MongoDB');
+} catch(error)  {console.error('Error al conectar a MongoDB:', error);
+process.exit (1);   
+}
+
+}
+
+
+
+//# Gabriel Lobo Varela
